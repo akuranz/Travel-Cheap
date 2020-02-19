f901ab54171561136ef59fd976f9c47813840aa3
Abby Kuranz <akuranz@gmail.com>
2020-02-18 19:29:42 -0700

    passport set up, added a database, added signup.js

--- public/js/searchResults.js
+++ public/js/searchResults.js
@@ -10,28 +10,35 @@ $("#searchCity").click(function() {
   var cityTo = $("#cityToInput")
     .val()
     .trim();
-  var departureDate = $("#departureDate")
-    .val()
-    .trim();
-  var returnDate = $("#returnDate")
-    .val()
-    .trim();
+  // var departureDate = $("#departureDate")
+  //   .val()
+  //   .trim();
+  // var returnDate = $("#returnDate")
+  //   .val()
+  //   .trim();
   // insert function to send to the backend for the axios call?
-  postCity(cityFrom, cityTo, departureDate, returnDate);
+  // postCity(cityFrom, cityTo, departureDate, returnDate);
+  postCity(cityFrom, cityTo);
 });
 
 // clicking previously searched itinerary
 $(document).on("click", ".city", function(event) {
   var cityFrom = $(this).attr("data-nameFrom");
   var cityTo = $(this).attr("data-nameTo");
-  var departureDate = $(this).attr("data-nameDeparture");
-  var returnDate = $(this).attr("data-nameReturn");
+  // var departureDate = $(this).attr("data-nameDeparture");
+  // var returnDate = $(this).attr("data-nameReturn");
   // insert function to send to the backend for the axios call?
-  postCity(cityFrom, cityTo, departureDate, returnDate);
+  // postCity(cityFrom, cityTo, departureDate, returnDate);
+  postCity(cityFrom, cityTo);
 });
 
-function postCity(cityFrom, cityTo, departureDate, returnDate) {
-  cities = { cityFrom: cityFrom, cityTo: cityTo, departureDate: departureDate, returnDate, returnDate };
+function postCity(cityFrom, cityTo) {
+  cities = {
+    cityFrom: cityFrom,
+    cityTo: cityTo
+    // departureDate: departureDate,
+    // returnDate: returnDate
+  };
   $.post("/api/citySearch", cities).then(function(data) {
     console.log(data);
     if (data) {
@@ -46,26 +53,25 @@ function postCity(cityFrom, cityTo, departureDate, returnDate) {
 // below is renderButtons function from weather planner that uses local storage to create buttons
 // will have to change to using database information
 
-// function renderButtons() {
-//     cities = JSON.parse(localStorage.getItem("savedCities"));
-//     if (cities === null || cities === undefined){
-//         return;
-//     }
-//     // Deleting the buttons prior to adding new cities
-//     $("#previousSearch").empty();
-//         // Looping through the array of cities
-//         console.log("Cities: " + cities);
-//         for (var i = 0; i < cities.length; i++) {
-
-//           // Then dynamicaly generating buttons for each city in the array
-//           var a = $("<button>");
-//           // Adding a class of movie to our button
-//           a.addClass("city btn btn-outline-secondary btn-block");
-//           // Adding a data-attribute
-//           a.attr("data-name", cities[i]);
-//           // Providing the initial button text
-//           a.text(cities[i]);
-//           // Adding the button to the buttons-view div
-//           $("#previousSearch").append(a);
-//         }
-// }
+function renderButtons() {
+  cities = JSON.parse(localStorage.getItem("savedCities"));
+  if (cities === null || cities === undefined) {
+    return;
+  }
+  // Deleting the buttons prior to adding new cities
+  $("#previousSearch").empty();
+  // Looping through the array of cities
+  console.log("Cities: " + cities);
+  for (var i = 0; i < cities.length; i++) {
+    // Then dynamicaly generating buttons for each city in the array
+    var a = $("<button>");
+    // Adding a class of movie to our button
+    a.addClass("city btn btn-outline-secondary btn-block");
+    // Adding a data-attribute
+    a.attr("data-name", cities[i]);
+    // Providing the initial button text
+    a.text(cities[i]);
+    // Adding the button to the buttons-view div
+    $("#previousSearch").append(a);
+  }
+}
