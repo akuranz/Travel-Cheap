fa571e15c1baeefaa88473a5a30917cf3b133ee6
Abby Kuranz <akuranz@gmail.com>
2020-02-26 19:01:04 -0700

    searchResults updated completely

--- public/js/searchResults.js
+++ public/js/searchResults.js
@@ -1,48 +1,154 @@
 // AirportInput("cityFromInput");
 // AirportInput("cityToInput");
 // clicking search button
-$("#searchCity").click(function() {
-  console.log("hi");
-  event.preventDefault();
-  let cityFrom = $("#cityFromInput")
-    .val()
-    .trim();
-  let cityTo = $("#cityToInput")
-    .val()
-    .trim();
-  let cityEvent = $("#cityEvent")
-    .val()
-    .trim();
-  let departureDate = $("#departureDate")
-    .val()
-    .trim();
-  let returnDate = $("#returnDate")
-    .val()
-    .trim();
-  // insert function to send to the backend for the axios call?
+console.log("searchResults js file");
+// $("#searchCity").click(function() {
+//   console.log("hi");
+//   event.preventDefault();
+//   let cityEvent = $("#cityEvent")
+//     .val()
+//     .trim();
+//   let cityFrom = $("#cityFrom")
+//     .val()
+//     .trim();
+//   let cityTo = $("#cityTo")
+//     .val()
+//     .trim();
+//   let departureDate = $("#departureDate")
+//     .val()
+//     .trim();
+//   let returnDate = $("#returnDate")
+//     .val()
+//     .trim();
+//   postCity(cityFrom, cityTo, departureDate, returnDate, cityEvent);
+// });
 
-  // postCity(cityFrom, cityTo, departureDate, returnDate);
-  // postCity(cityFrom, cityTo, cityEvent);
+// let saved = $(".change-saved");
+// saved.click(function() {
+//   let eventId = saved.data(eventid)
+//   saved.data("saved", true);
+// });
 
-  // original format YYYY-MM-DD, needs to change to MM/DD/YYYY
-  // takes depature and return date and reformats it
-  // let departureD = $("#departureDate")
-  //   .val()
-  //   .trim();
+let savedSearch = [];
+$(function() {
+  $(".change-saved").on("click", function(event) {
+    event.preventDefault();
+    var eventId = $(this).data("eventid");
+    var saved = $(this).data("saved");
+    // saved = $(this).attr("saved", false);
+    console.log("eventId", eventId);
+    console.log("saved", saved);
+    console.log(typeof saved);
+    if (saved === true) {
+      for (i = 0; i < savedSearch.length; i++) {
+        //loop through array
+        savedSearch.push(saved); //push variables
+      }
+    }
+    console.log(savedSearch);
 
-  // const departureDate = dateFormatter(departureD);
-
-  // console.log("Departure Date: ", departureDate.moment().format("DD/MM/YYYY"));
-  //   let returnD = $("#returnDate")
-  //     .val()
-  //     .trim();
-
-  console.log("Departure Date: ", departureDate);
-  // const returnDate = dateFormatter(returnD);
-
-  console.log("Return Date: ", returnDate);
-  postCity(cityFrom, cityTo, departureDate, returnDate);
+    // var newSaved = $(this).data("eventid", true);
+    // console.log(newSaved);
+  });
 });
+
+// function postCity(cityFrom, cityTo, departureDate, returnDate, cityEvent) {
+//   console.log("In post flight");
+//   cities = {
+//     cityFrom: cityFrom,
+//     cityTo: cityTo,
+//     departureDate: departureDate,
+//     returnDate: returnDate,
+//     cityEvent: cityEvent
+//   };
+//   $.post("/api/citySearch", cities)
+//     .then(function(data) {
+//       console.log(data);
+//       if (data) {
+//         console.log("You looked up: ", cities);
+//       } else {
+//         console.log("That is not a valid city");
+//       }
+//     })
+//     .catch(function(err) {
+//       console.log(err.message);
+//     });
+// }
+
+//this is where we create the nested object?
+//create container with events and flights in index.handlebars
+const saveItinerary = $(".saveItinerary");
+//get the object
+// saveItinerary.on(() => {
+//   event.preventDefault();
+// });
+
+// function saveItinerary(trip, flights, events) {
+//   //need an array on the front end in the right format
+//   savedItineraries = {
+//     trip: {
+//       cityName: cityEvent,
+//       departureDate: departureDate,
+//       arrivalDate: arrivalDate
+//     },
+//     flights: [
+//       {
+//         price: price,
+//         departureDate: departureDate,
+//         arrivalDate: arrivalDate
+//       },
+//       {
+//         price: price,
+//         departureDate: departureDate,
+//         arrivalDate: arrivalDate
+//       }
+//     ],
+//     events: [
+//       {
+//         name: eventName,
+//         date: eventDate,
+//         time: eventTime
+//       },
+//       {
+//         name: eventName,
+//         date: eventDate,
+//         time: eventTime
+//       },
+//       {
+//        name: eventName,
+//         date: eventDate,
+//         time: eventTime
+//       }
+//     ]
+//   };
+//   $.post("api/trips", savedItineraries).then(function(data) {
+//     console.log(data);
+//     if (data) {
+//       console.log("You looked up: ", cities);
+//     } else {
+//       console.log("That is not a valid city");
+//     }
+//   });
+// }
+
+// original format YYYY-MM-DD, needs to change to MM/DD/YYYY
+// takes depature and return date and reformats it
+// let departureD = $("#departureDate")
+//   .val()
+//   .trim();
+
+// const departureDate = dateFormatter(departureD);
+
+// console.log("Departure Date: ", departureDate.moment().format("DD/MM/YYYY"));
+//   let returnD = $("#returnDate")
+//     .val()
+//     .trim();
+
+// console.log("Departure Date: ", departureDate);
+// const returnDate = dateFormatter(returnD);
+
+// console.log("Return Date: ", returnDate);
+
 // clicking previously searched itinerary
 // $(document).on("click", ".city", function(event) {
 //   var cityFrom = $(this).attr("data-nameFrom");
@@ -51,11 +157,9 @@ $("#searchCity").click(function() {
 //   // var returnDate = $(this).attr("data-nameReturn");
 //   // insert function to send to the backend for the axios call?
 
-
-  // postCity(cityFrom, cityTo, departureDate, returnDate);
-  postCity(cityFrom, cityTo, cityEvent);
-});
-
+// postCity(cityFrom, cityTo, departureDate, returnDate);
+//   postCity(cityFrom, cityTo, cityEvent);
+// });
 
 // function postCity(cityFrom, cityTo) {
 //   console.log("In post city")
@@ -83,28 +187,6 @@ $("#searchCity").click(function() {
 //   // function to send to the backend
 //   postCity(cityFrom, cityTo, departureDate, returnDate);
 // }
-
-function postCity(cityFrom, cityTo, cityEvent, departureDate, returnDate) {
-  console.log("In post city");
-  // cities = { cityFrom: cityFrom, cityTo: cityTo };
-  // function postCity(cityFrom, cityTo, departureDate, returnDate) {
-  cities = {
-    cityFrom: cityFrom,
-    cityTo: cityTo,
-    departureDate: departureDate
-    returnDate:
-    returnDate,
-    cityEvent: cityEvent
-  };
-  $.post("/api/citySearch", cities).then(function(data) {
-    console.log(data);
-    if (data) {
-      console.log("You looked up: ", cities);
-    } else {
-      console.log("That is not a valid city");
-    }
-  });
-}
 
 //reformatted to move day first, then month to match api query
 // function dateFormatter(date) {
